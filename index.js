2ca0c2023004a11f13d170b0bf1e7cdfe3f4a584
Justin <vien_justin@hotmail.com>
2022-04-20 14:06:16 -0400

    added more routing

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const express = require('express')
+// const morgan = require('morgan')
 const app = express()
+const cors = require('cors')
+
+app.use(express.json())
+app.use(cors())
+
+
 
 let persons = [
     { 
@@ -49,7 +56,40 @@ app.get('/api/persons/:id', (request, response) => {
     }
 })
 
-// app.delete('/api/persons/:id', )
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(person => person.id !== id)
+
+    response.status(204).end();
+})
+
+const generateId = () => {
+    const maxId = persons.length > 0
+    ? Math.max(...persons.map(person => person.id))
+    : 0
+    return maxId + 1
+}
+
+app.post('/api/persons', (request, response) => {
+    
+    const body = request.body 
+
+    if (!body.content) {
+        return response.status(400).json({
+            error: 'content missing'
+        })
+    }
+
+    const person = {
+        content: body.content,
+        date: new Date(),
+        id: generateId()
+    }
+
+    persons = persons.concat(person)
+    console.log(person)
+    response.json(person)
+})
 
 const PORT = 3001
 app.listen(PORT, () => {
